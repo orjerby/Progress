334f67c4c41838af835e1fe0d49df874101f1c28
orjerby <orjerby@gmail.com>
2019-03-24 21:44:04 +0200

    Setup ProjectList component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router, Route, Switch, Link } from 'react-router-dom'
 
 import history from '../history'
-import Home from './Home'
+import ProjectList from './ProjectList'
 import Login from './Login'
 
 export default () => {
@@ -17,7 +17,7 @@ export default () => {
 
                     {/* our routes. Switch only render the first route that matches the url */}
                     <Switch>
-                        <Route path="/" exact component={Home} />
+                        <Route path="/" exact component={ProjectList} />
                         <Route path="/login" exact component={Login} />
                     </Switch>
                 </div>
